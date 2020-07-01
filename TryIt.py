e3bc9a71afd83aa6c7820c06e84b9d4f1bd9e9d5
ZT C <b81426@gmail.com>
2020-07-01 09:31:34 +0800

    try to change entry text

--- TryIt.py
+++ TryIt.py
@@ -49,8 +49,27 @@ root = tk.Tk()
 # x = Dates_BQ.cget("text")
 # print(x)
 
-Caculate_Orient = tk.Button(root, text = "Go！").grid(row = 2, column = 9, padx = 5, pady = 5)
 
+
+
+
+InputNPQ = tk.IntVar()
+InputNPQ.set(1)
+InputNHQ = tk.IntVar()
+InputNHQ.set(0)
+InputEPQ = tk.IntVar()
+InputEPQ.set(0)
+InputEHQ = tk.IntVar()
+InputEHQ.set(0)
+Nomads_PQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputNPQ).grid(row = 1, column = 4, padx = 5, pady = 5)
+Nomads_HQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputNHQ).grid(row = 2, column = 4, padx = 5, pady = 5)
+Envoys_PQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputEPQ).grid(row = 1, column = 8, padx = 5, pady = 5)
+Envoys_HQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputEHQ).grid(row = 2, column = 8, padx = 5, pady = 5)
+
+
+
+
+Caculate_Orient = tk.Button(root, text = "Go！").grid(row = 2, column = 9, padx = 5, pady = 5)
 
 
 root.mainloop()
