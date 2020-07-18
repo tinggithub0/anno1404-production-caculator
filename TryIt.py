645fdd0252440f9989c350b59b68957306402b99
ZT C <b81426@gmail.com>
2020-07-18 09:07:32 +0800

    PQ.HQ convert by Radiobutton

--- TryIt.py
+++ TryIt.py
@@ -13,6 +13,8 @@ root = tk.Tk()
 # x = Rbtn1.get()
 # print(x)
 
+
+
 ### Entry value get ###
 # InputNPQ = tk.IntVar()
 # InputNPQ.set(1)
@@ -30,6 +32,8 @@ root = tk.Tk()
 # x = InputNPQ.get()
 # print(x)
 
+
+
 ### Combobox value get ###
 # DatesPersent = tk.StringVar()
 # ChosenValues = ["0%", "25%", "50%", "75%"]
@@ -42,12 +46,15 @@ root = tk.Tk()
 # print(x)
 
 
+
 ### label text cget ###
 # DatesBQ = tk.IntVar()
 # Dates_BQ = tk.Label(root, width = 5, bd = 3, text = "2")
 # Dates_BQ.grid(row = 0, column = 2, padx = 5, pady = 5)
 # x = Dates_BQ.cget("text")
 # print(x)
+
+
 
 ### PQ.HQ convert ###
 # def convert(InputNHQ):
@@ -68,12 +75,43 @@ root = tk.Tk()
 # Envoys_PQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputEPQ).grid(row = 1, column = 8, padx = 5, pady = 5)
 # Envoys_HQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputEHQ).grid(row = 2, column = 8, padx = 5, pady = 5)
 
-
 # Caculate_Orient = tk.Button(root, text = "Go！").grid(row = 2, column = 9, padx = 5, pady = 5)
 
 
 
+### PQ.HQ convert by Radiobutton ###
+# def convert0(Rbtn1):
+#     Rbtn1 = Rbtn1.get()
+#     # print(Rbtn1)
 
+# Rbtn1 = tk.IntVar()
+# # Rbtn1.set(1)
+# RadioP = tk.Radiobutton(root, value = 1, variable = Rbtn1).grid(row = 1, column = 0, padx = 5, pady = 5)
+# RadioH = tk.Radiobutton(root, value = 2, variable = Rbtn1).grid(row = 2, column = 0, padx = 5, pady = 5)
+# Rbtn1.trace("w", lambda name, index, mode, Rbtn1 = Rbtn1: convert0(Rbtn1))
+
+# def convert(*args):
+#     if Rbtn1.get() == 1:
+#         InputNHQ.set((InputNPQ.get())/15)
+#         # print(Rbtn1.get())
+#     else:
+#         InputNPQ.set((InputNHQ.get())*15)
+#         # print(Rbtn1.get())
+
+# InputNPQ = tk.IntVar()
+# InputNPQ.set(0)
+# InputNPQ.trace("w", lambda name, index, mode, InputNPQ = InputNPQ: convert(InputNPQ))
+# InputNHQ = tk.IntVar()
+# InputNHQ.set(0)
+# InputNHQ.trace("w", lambda name, index, mode, InputNHQ = InputNHQ: convert(InputNHQ))
+# InputEPQ = tk.IntVar()
+# InputEPQ.set(0)
+# InputEHQ = tk.IntVar()
+# InputEHQ.set(0)
+# Nomads_PQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputNPQ).grid(row = 1, column = 4, padx = 5, pady = 5)
+# Nomads_HQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputNHQ).grid(row = 2, column = 4, padx = 5, pady = 5)
+# Envoys_PQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputEPQ).grid(row = 1, column = 8, padx = 5, pady = 5)
+# Envoys_HQEntry = tk.Entry(root, width = 6, bd = 3, textvariable = InputEHQ).grid(row = 2, column = 8, padx = 5, pady = 5)
 
 
 
